61a43e8eac4ad27eb870b7b717a8da65479e0322
Drew Cook <dc333@usf.edu>
2025-02-02 21:20:39 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -3,3 +3,9 @@ let prices = [5, 7.50, 10, 15, 35]; //declare array
 prices.push(40); //add value to array
 prices.shift(); //remove the first instance
 console.log(prices); //display the updated array
+
+//Task 2 Modifying customer orders scenario
+let orders = [2, 3, 8, 4, 12, 9]; //define array
+orders[2] += 5; //add 5 to the 3rd order
+console.log(`Number of orders: ${orders.reduce((total, amount) => total + amount, 0)}
+Orders: ${orders}`); //display the number of instances, and the array of orders
