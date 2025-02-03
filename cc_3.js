ac9fd308ffdb67ee84b67b8e1dcec69f67300d64
Drew Cook <dc333@usf.edu>
2025-02-02 20:57:24 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,5 @@
+//Task 1 Product price management scenario
+let prices = [5, 7.50, 10, 15, 35]; //declare array
+prices.push(40); //add value to array
+prices.shift(); //remove the first instance
+console.log(prices); //display the updated array
